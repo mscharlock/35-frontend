be4b12ca4e4b110dbcf720648d27a3932360286a
mscharlock <michelle.scharlock@gmail.com>
2017-10-13 14:51:13 -0700

    Semicolons:

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use(express.static(`${__dirname}/build`))
-app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
+app.use(express.static(`${__dirname}/build`));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
-app.listen(process.env.PORT, () => console.log(`SERVER RUNNING ON: ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`SERVER RUNNING ON: ${process.env.PORT}`));
